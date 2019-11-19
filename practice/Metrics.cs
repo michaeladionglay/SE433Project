b0945bf961c6fb0e0f39f7d885d515c266857d1a
Michaela Dionglay <mia_dionglay@yahoo.com>
2019-11-18 20:54:27 -0600

    added vers in practice folder and changed path to parent path

--- practice/Metrics.cs
+++ practice/Metrics.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Project_MJ
 {
@@ -13,8 +14,9 @@ namespace Project_MJ
             string metrics_dir = "C:\\Users\\mia_d\\.nuget\\packages\\microsoft.codeanalysis.metrics\\2.9.6\\Metrics\\";
 
             int versionCount;
-            for(versionCount = 0; versionCount <= dataList.Length; versionCount++)
+            for(versionCount = 0; versionCount < dataList.Length; versionCount++)
             {
+                File.Delete("C:\\Users\\mia_d\\Desktop\\reports\\Version_" + versionCount + ".xml");
                 System.Diagnostics.Process.Start("CMD.exe", "/C " + metrics_dir + "Metrics.exe /p:" + dataList[versionCount] + "Newtonsoft.Json.csproj /o:C:\\Users\\mia_d\\Desktop\\reports\\Version_" + versionCount + ".xml");
             }
         }
--- practice/Program.cs
+++ practice/Program.cs
@@ -7,16 +7,18 @@ namespace Project_MJ
     {
         static void Main(string[] args)
         {
-            string beginning = "C:\\Users\\mia_d\\Desktop\\Newtonsoft_Vers\\";
+            string workingDirectory = Environment.CurrentDirectory;
+            string beginning = Directory.GetParent(workingDirectory).Parent.FullName+"\\newtonsoft_Vers\\";
             string[] VersionList = {
-                beginning+"Newtonsoft.Json-2.0.1\\Src\\Newtonsoft.Json\\",
+
+                beginning+"Newtonsoft.Json-2.0.2\\Src\\Newtonsoft.Json\\",
                 beginning+"Newtonsoft.Json-2.0.3\\Src\\Newtonsoft.Json\\",
                 beginning+"Newtonsoft.Json-2.0.4\\Src\\Newtonsoft.Json\\"
                 };
-            
-            
+
             Metrics Analysis = new Metrics();
             Analysis.RunMetrics(VersionList);
+            //Console.ReadKey();
         }
     }
 }
