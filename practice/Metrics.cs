5c5c9b43ed65441bbb19d2066eb182c191a93b3f
Michaela Dionglay <mia_dionglay@yahoo.com>
2019-11-18 23:44:32 -0600

    Able to collect data and set it to attributes for each object

--- practice/Metrics.cs
+++ practice/Metrics.cs
@@ -4,21 +4,109 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Xml;
+using System.Diagnostics;
 
 namespace Project_MJ
 {
     class Metrics
     {
-        public void RunMetrics(string [] dataList)
+        private int MaintainabilityIndex { get; set; }
+        private int CyclomaticComplexity { get; set; }
+        private int ClassCoupling { get; set; }
+        private int DepthOfInheritance { get; set; }
+        private int SourceLines { get; set; }
+        private int ExecutableLines { get; set; }
+        private string output { get; set; }
+        public void RunMetrics(string Version, int num)
         {
             string metrics_dir = "C:\\Users\\mia_d\\.nuget\\packages\\microsoft.codeanalysis.metrics\\2.9.6\\Metrics\\";
 
+            File.Delete("C:\\Users\\mia_d\\Desktop\\reports\\Version_" + num + ".xml");
+            string input = Version + "Newtonsoft.Json.csproj";
+            this.output = "C:\\Users\\mia_d\\Desktop\\reports\\Version_" + num + ".xml";
+            Process processID = Process.Start("CMD.exe", "/C " + metrics_dir + "Metrics.exe /p:" + input + " /o:" + this.output);
+
+            while (!processID.HasExited) { }
+            /*
             int versionCount;
             for(versionCount = 0; versionCount < dataList.Length; versionCount++)
             {
                 File.Delete("C:\\Users\\mia_d\\Desktop\\reports\\Version_" + versionCount + ".xml");
                 System.Diagnostics.Process.Start("CMD.exe", "/C " + metrics_dir + "Metrics.exe /p:" + dataList[versionCount] + "Newtonsoft.Json.csproj /o:C:\\Users\\mia_d\\Desktop\\reports\\Version_" + versionCount + ".xml");
             }
+            */
         }
+
+        public void extractAndSetData()
+        {
+            //"C:\\Users\\mia_d\\Desktop\\reports\\Version_" + num + ".xml"
+
+            XmlTextReader reader = new XmlTextReader(this.output);
+
+            //reader.MoveToContent();
+            reader.ReadToDescendant("Metrics");
+            Boolean isFull = false;
+            while (reader.Read() && !isFull)
+            {
+                while (reader.MoveToNextAttribute())
+                {
+                    if(reader.Value == "MaintainabilityIndex")
+                    {
+                        reader.MoveToNextAttribute();
+                        this.MaintainabilityIndex = Int32.Parse(reader.Value);
+
+                    }
+
+                    if (reader.Value == "CyclomaticComplexity")
+                    {
+                        reader.MoveToNextAttribute();
+                        this.CyclomaticComplexity = Int32.Parse(reader.Value);
+                    }
+
+                    if (reader.Value == "ClassCoupling")
+                    {
+                        reader.MoveToNextAttribute();
+                        this.ClassCoupling = Int32.Parse(reader.Value);
+                    }
+
+                    if (reader.Value == "DepthOfInheritance")
+                    {
+                        reader.MoveToNextAttribute();
+                        this.DepthOfInheritance = Int32.Parse(reader.Value);
+                    }
+
+                    if (reader.Value == "SourceLines")
+                    {
+                        reader.MoveToNextAttribute();
+                        this.SourceLines = Int32.Parse(reader.Value);
+                    }
+
+                    if (reader.Value == "ExecutableLines")
+                    {
+                        reader.MoveToNextAttribute();
+                        this.ExecutableLines = Int32.Parse(reader.Value);
+                        isFull = true;
+                    }
+
+                   
+                }
+                
+                if (isFull)
+                {
+                    Console.WriteLine("Maintainability: " + this.MaintainabilityIndex);
+                    Console.WriteLine("CC: " + this.CyclomaticComplexity);
+                    Console.WriteLine("ClassCoupling: " + this.ClassCoupling);
+                    Console.WriteLine("DofI: " + this.DepthOfInheritance);
+                    Console.WriteLine("SourceLines: " + this.SourceLines);
+                    Console.WriteLine("ExecutableLines: " + this.ExecutableLines);
+                    break;
+                }
+            }
+            Console.ReadKey();
+
+        }
+
+
     }
 }
--- practice/Program.cs
+++ practice/Program.cs
@@ -9,16 +9,25 @@ namespace Project_MJ
         {
             string workingDirectory = Environment.CurrentDirectory;
             string beginning = Directory.GetParent(workingDirectory).Parent.FullName+"\\newtonsoft_Vers\\";
-            string[] VersionList = {
-
+            string[] VersionList = 
+            {
                 beginning+"Newtonsoft.Json-2.0.2\\Src\\Newtonsoft.Json\\",
                 beginning+"Newtonsoft.Json-2.0.3\\Src\\Newtonsoft.Json\\",
                 beginning+"Newtonsoft.Json-2.0.4\\Src\\Newtonsoft.Json\\"
-                };
+            };
 
-            Metrics Analysis = new Metrics();
-            Analysis.RunMetrics(VersionList);
-            //Console.ReadKey();
+            Metrics[] dataList = new Metrics[25];
+
+            int i;
+            for(i = 0; i < VersionList.Length; i++)
+            {
+                Metrics Analysis = new Metrics();
+                Analysis.RunMetrics(VersionList[i], i);
+                Analysis.extractAndSetData();
+                dataList[i] = Analysis;
+            }
+
+
         }
     }
 }
